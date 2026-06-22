1f957e2931935ebb064303d3b9e2e2464e8c0300
kon2raya24 <konturaya@gmail.com>
2026-06-22 09:03:17 +0000

    docs: add JSDoc comments to validatephilhealth.ts

--- src/validatephilhealth.ts
+++ src/validatephilhealth.ts
@@ -1,6 +1,10 @@
 
 // Type exports for consumers
 // Add specific types as needed
+/**
+ * validatePhilHealth
+ * @param num
+ */
 export function validatePhilHealth(num: string): boolean {
   const cleaned = num.replace(/[^0-9]/g, "");
   return /^\d{12}$/.test(cleaned);
